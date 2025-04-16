2c14b06f6cec9dbe9c9afd53bffdb28396f57716
unknown <danielzadva@gmail.com>
2025-04-16 22:26:02 +0100

    exercise1 completed

--- basic/exercise1.js
+++ basic/exercise1.js
@@ -1,10 +1,17 @@
 //JS Script to check if a number is odd or even
-let number_input = Number(prompt("Enter a number"));
+let number_input = prompt("Enter a number");
 
-if (number_input % 2 == 0) {
-    alert("The number is even");
-} else if (number_input % 2 != 0) {
-    alert("The number is odd");
+if(number_input === null || number_input.trim() === "") {
+    alert("No Inputs Provided!");
+} else if(isNaN(number_input)) {
+    alert("Input Not a Number!")
 } else {
-    alert("Invalid input!")
+    let number = Number(number_input)
+    if (number % 2 == 0) {
+        alert("The number is even");
+    } else if (number % 2 != 0) {
+        alert("The number is odd");
+    } else {
+        alert("Invalid input!")
+    }
 }
